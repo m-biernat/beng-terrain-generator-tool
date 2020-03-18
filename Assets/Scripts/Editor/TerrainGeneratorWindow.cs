b1ae209d8b2a1e8cec0bebb681a55fc3309e0f30
m-bt <44023124+m-bt@users.noreply.github.com>
2020-03-18 19:46:24 +0100

    Added editor window with basic layout

---
+++ Assets/Scripts/Editor/TerrainGeneratorWindow.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class TerrainGeneratorWindow : EditorWindow
+{
+    private Toolbar toolbar = Toolbar.General;
+
+    private readonly string[] toolbarOptions =
+    {
+        "General", "Heightmap", "Erosion", "Texturing", "Detail"
+    };
+
+    private bool autoGenerate = false;
+    private Generate generate = Generate.Heightmap;
+
+    [MenuItem("Window/Terrain Generator")]
+    public static void ShowWindow()
+    {
+        GetWindow<TerrainGeneratorWindow>("Terrain Generator");
+    }
+
+    private void OnGUI()
+    {
+        toolbar = (Toolbar)GUILayout.Toolbar((int)toolbar, toolbarOptions, GUILayout.MinHeight(25.0f));
+
+        switch (toolbar)
+        {
+            case Toolbar.General:
+                General();
+                break;
+
+            case Toolbar.Heightmap:
+                Heightmap();
+                break;
+        }
+
+        GUILayout.FlexibleSpace();
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("[STATUS TEXT PH]", GUILayout.Height(35.0f));
+        GUILayout.FlexibleSpace();
+
+        GUILayout.BeginVertical();
+        autoGenerate = GUILayout.Toggle(autoGenerate, "Auto generate");
+        generate = (Generate)EditorGUILayout.EnumPopup(generate, GUILayout.Width(100.0f));
+        GUILayout.EndVertical();
+
+        GUILayout.Space(2.5f);
+
+        if (GUILayout.Button("Generate", GUILayout.Height(35.0f), GUILayout.Width(100.0f)))
+        {
+            // OnClick
+        }
+
+        GUILayout.EndHorizontal();
+    }
+
+    private void General()
+    {
+        GUILayout.Label("General");
+    }
+
+    private void Heightmap()
+    {
+        GUILayout.Label("Heightmap");
+    }
+
+    private enum Toolbar
+    {
+        General,
+        Heightmap
+    }
+
+    private enum Generate
+    {
+        TerrainOnly,
+        Heightmap,
+        Combined
+    }
+}
